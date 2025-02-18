12c9955998fd95f8c2d46beb2b680a6c55987e22
beccyXx <rebecca.kluge94@gmail.com>
2025-02-18 14:11:25 +0100

    logs

--- static/js/delaunaySketchRandom.js
+++ static/js/delaunaySketchRandom.js
@@ -101,6 +101,7 @@ function draw() {
         strokeWeight(4);
         point(v.x, v.y);
     }
+    console.log("seedPoints:", seedPoints);
 
     // Zeichne die Dreiecke mit Farben aus der Farbpalette
     let { points, triangles } = delaunay;
@@ -129,6 +130,7 @@ function draw() {
 }
 
 function calculateDelaunay(points) {
+    console.log("points:", points);
     let pointsArray = [];
     for (let v of points) {
         pointsArray.push(v.x, v.y);
