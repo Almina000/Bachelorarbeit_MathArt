ee3973693d9c6b2ca18af40b2f1fa36a32c1b795
beccyXx <rebecca.kluge94@gmail.com>
2025-02-18 10:26:41 +0100

    .js window change

--- static/js/data/elbenwald_hashtags_count.js
+++ static/js/data/elbenwald_hashtags_count.js
@@ -1,134 +1,137 @@
-window.elbenwald_hashtags = [
-  {
-    "hashtag": "#ramenbowl",
-    "count": 1
-  },
-  {
-    "hashtag": "#starwarskitchen",
-    "count": 1
-  },
-  {
-    "hashtag": "#starwarsmerch",
-    "count": 1
-  },
-  {
-    "hashtag": "#starwarsfans",
-    "count": 1
-  },
-  {
-    "hashtag": "#elbenwald",
-    "count": 8
-  },
-  {
-    "hashtag": "#godzillafans",
-    "count": 1
-  },
-  {
-    "hashtag": "#godzillamerch",
-    "count": 1
-  },
-  {
-    "hashtag": "#loungefly",
-    "count": 1
-  },
-  {
-    "hashtag": "#rucksackliebe",
-    "count": 1
-  },
-  {
-    "hashtag": "#liloandstitch",
-    "count": 1
-  },
-  {
-    "hashtag": "#stitchfigur",
-    "count": 1
-  },
-  {
-    "hashtag": "#liloandstitchfans",
-    "count": 1
-  },
-  {
-    "hashtag": "#disneymerch",
-    "count": 1
-  },
-  {
-    "hashtag": "#sanrioaesthetic",
-    "count": 1
-  },
-  {
-    "hashtag": "#Sanriocharacters",
-    "count": 1
-  },
-  {
-    "hashtag": "#sanriomerch",
-    "count": 1
-  },
-  {
-    "hashtag": "#sanriofans",
-    "count": 1
-  },
-  {
-    "hashtag": "#kawaiiaesthetic",
-    "count": 1
-  },
-  {
-    "hashtag": "#dietageb\u00fccherderapothekerin",
-    "count": 1
-  },
-  {
-    "hashtag": "#theapothecarydiaries",
-    "count": 1
-  },
-  {
-    "hashtag": "#kusuriyanohitorigoto",
-    "count": 1
-  },
-  {
-    "hashtag": "#animefans",
-    "count": 2
-  },
-  {
-    "hashtag": "#animemerch",
-    "count": 2
-  },
-  {
-    "hashtag": "#harrypottermerch",
-    "count": 1
-  },
-  {
-    "hashtag": "#harrypotterzauberst\u00e4be",
-    "count": 1
-  },
-  {
-    "hashtag": "#severussnape",
-    "count": 1
-  },
-  {
-    "hashtag": "#afterallthistimealways",
-    "count": 1
-  },
-  {
-    "hashtag": "#onepiece",
-    "count": 1
-  },
-  {
-    "hashtag": "#onepiecefans",
-    "count": 1
-  },
-  {
-    "hashtag": "#onepiecemerch",
-    "count": 1
-  },
-  {
-    "hashtag": "#sockenliebe",
-    "count": 1
-  },
-  {
-    "hashtag": "#figurensammlung",
-    "count": 1
-  },
-  {
-    "hashtag": "#herrderringefans",
-    "count": 1
-  }
-];
+if (typeof window !== 'undefined') {
+  window.elbenwald_hashtags = [
+    {
+      "hashtag": "#ramenbowl",
+      "count": 1
+    },
+    {
+      "hashtag": "#starwarskitchen",
+      "count": 1
+    },
+    {
+      "hashtag": "#starwarsmerch",
+      "count": 1
+    },
+    {
+      "hashtag": "#starwarsfans",
+      "count": 1
+    },
+    {
+      "hashtag": "#elbenwald",
+      "count": 8
+    },
+    {
+      "hashtag": "#godzillafans",
+      "count": 1
+    },
+    {
+      "hashtag": "#godzillamerch",
+      "count": 1
+    },
+    {
+      "hashtag": "#loungefly",
+      "count": 1
+    },
+    {
+      "hashtag": "#rucksackliebe",
+      "count": 1
+    },
+    {
+      "hashtag": "#liloandstitch",
+      "count": 1
+    },
+    {
+      "hashtag": "#stitchfigur",
+      "count": 1
+    },
+    {
+      "hashtag": "#liloandstitchfans",
+      "count": 1
+    },
+    {
+      "hashtag": "#disneymerch",
+      "count": 1
+    },
+    {
+      "hashtag": "#sanrioaesthetic",
+      "count": 1
+    },
+    {
+      "hashtag": "#Sanriocharacters",
+      "count": 1
+    },
+    {
+      "hashtag": "#sanriomerch",
+      "count": 1
+    },
+    {
+      "hashtag": "#sanriofans",
+      "count": 1
+    },
+    {
+      "hashtag": "#kawaiiaesthetic",
+      "count": 1
+    },
+    {
+      "hashtag": "#dietageb\u00fccherderapothekerin",
+      "count": 1
+    },
+    {
+      "hashtag": "#theapothecarydiaries",
+      "count": 1
+    },
+    {
+      "hashtag": "#kusuriyanohitorigoto",
+      "count": 1
+    },
+    {
+      "hashtag": "#animefans",
+      "count": 2
+    },
+    {
+      "hashtag": "#animemerch",
+      "count": 2
+    },
+    {
+      "hashtag": "#harrypottermerch",
+      "count": 1
+    },
+    {
+      "hashtag": "#harrypotterzauberst\u00e4be",
+      "count": 1
+    },
+    {
+      "hashtag": "#severussnape",
+      "count": 1
+    },
+    {
+      "hashtag": "#afterallthistimealways",
+      "count": 1
+    },
+    {
+      "hashtag": "#onepiece",
+      "count": 1
+    },
+    {
+      "hashtag": "#onepiecefans",
+      "count": 1
+    },
+    {
+      "hashtag": "#onepiecemerch",
+      "count": 1
+    },
+    {
+      "hashtag": "#sockenliebe",
+      "count": 1
+    },
+    {
+      "hashtag": "#figurensammlung",
+      "count": 1
+    },
+    {
+      "hashtag": "#herrderringefans",
+      "count": 1
+    }
+  ];
+  console.log("Hashtag-Daten erfolgreich geladen:", window.elbenwald_hashtags);
+}
